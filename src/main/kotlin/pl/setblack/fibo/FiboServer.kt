bf7730a82203fca8f583603025c33846b6acd584
jarekratajski <jratajski@gmail.com>
2019-04-11 22:29:42 +0200

    full solution

--- src/main/kotlin/pl/setblack/fibo/FiboServer.kt
+++ src/main/kotlin/pl/setblack/fibo/FiboServer.kt
@@ -1,5 +1,6 @@
 package pl.setblack.fibo
 
+import io.netty.channel.nio.NioEventLoopGroup
 import org.springframework.http.MediaType
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter
 import org.springframework.web.reactive.function.BodyInserters.fromObject
@@ -9,8 +10,8 @@ import org.springframework.web.reactive.function.server.RouterFunctions
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Mono
-import reactor.ipc.netty.http.server.HttpServer
-import java.util.function.Function
+import reactor.netty.http.server.HttpServer
+import reactor.netty.tcp.TcpServer
 
 class FiboServer {
     fun start() {
@@ -18,7 +19,9 @@ class FiboServer {
             GET("/fib/{n}")
             { request ->
                 val n = Integer.parseInt(request.pathVariable("n"))
+                println("Thread: "+ Thread.currentThread().name)
                 if (n < 2) {
+
                     ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromObject<String>(n.toString()))
                 } else {
                     val n_1 = WebClient.create("http://localhost:8080").get().uri("/fib/{n}", n - 1)
@@ -30,17 +33,32 @@ class FiboServer {
                             .map<Int> { Integer.parseInt(it) }
 
                     val result = n_1
-                            .flatMap { a -> n_2.map { b -> a!! + b!! } }
+                            .flatMap { a -> n_2.map { b -> a +b   } }
                             .map<String>( { it.toString() })
-                    ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromPublisher<String, Mono<String>>(result, String::class.java))
+                    ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(
+                            fromPublisher<String, Mono<String>>(result, String::class.java))
                 }
             }
         }
 
         val httpHandler = RouterFunctions.toHttpHandler(route)
         val adapter = ReactorHttpHandlerAdapter(httpHandler)
-        val server = HttpServer.create("localhost", 8080)
-        server.startAndAwait(adapter)
+        val eventLoopGroup = NioEventLoopGroup(300)
+        val tcpServer = TcpServer
+                .create()
+                .host("0.0.0.0")
+                .port(8080)
+                .runOn(eventLoopGroup)
+
+        val server = HttpServer
+                .from(tcpServer)
+                .handle(adapter)
+                .bind()
+                .block()!!
+
+        readLine()
+
+        server.disposeNow()
     }
 }
 
